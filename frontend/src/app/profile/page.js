f8ef30152e7a44e03ffde326b0c5871e45dc195a
rampop01 <popoolarahmat96@gmail.com>
2026-06-18 21:45:03 +0100

    a11y(core): improve screen reader accessibility with aria-label

--- frontend/src/app/profile/page.js
+++ frontend/src/app/profile/page.js
@@ -145,7 +145,7 @@ export default function Profile() {
                     </div>
                   ))}
                   <div className="w-16 h-16 border-2 border-neutral-800 rotate-45 flex items-center justify-center bg-black">
-                    <div className="-rotate-45 text-neutral-600 font-bold">?</div>
+                    <div aria-label="Interactive element cae0" className="-rotate-45 text-neutral-600 font-bold">?</div>
                   </div>
                 </div>
               </div>
