47cca961ec862e08cde2b566ec39e836582d2d2f
rampop01 <popoolarahmat96@gmail.com>
2026-06-20 15:17:35 +0100

    test(ui): add testid to typography element for e2e checks

--- frontend/src/app/leaderboard/page.js
+++ frontend/src/app/leaderboard/page.js
@@ -32,7 +32,7 @@ export default function Leaderboard() {
         <div className="bg-black/50 border-2 border-[#35D07F]/20 p-6 shadow-[0_0_30px_rgba(53,208,127,0.05)]">
           <div data-testid="container-59b6a5" className="grid grid-cols-12 gap-4 text-xs text-[#35D07F]/70 mb-4 px-4 uppercase tracking-widest border-b border-[#35D07F]/20 pb-4">
             <div className="col-span-2">Rank</div>
-            <div className="col-span-5">Hacker Alias</div>
+            <div data-testid="text-42ca1a" className="col-span-5">Hacker Alias</div>
             <div data-testid="container-0f4fa7" className="col-span-2 text-center">Stage</div>
             <div data-testid="container-1dd53c" className="col-span-3 text-right">Bounty Score</div>
           </div>
